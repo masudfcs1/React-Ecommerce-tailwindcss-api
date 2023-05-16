0781256705fbbcfaeb02b105e458c261299776c7
masudfcs1 <masudfcs1@gmail.com>
2023-05-16 13:11:58 +0600

    Product Details

--- src/components/CartItem.jsx
+++ src/components/CartItem.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from "react";
+import React, { useContext } from "react";
 import { Link } from "react-router-dom";
 import { IoMdAdd, IoMdClose, IoMdRemove } from "react-icons/io";
 import { CartContext } from "../contexts/CartContext";
